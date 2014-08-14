31f951e13be0de0300b43afb73987ff85390e619
djunior_s@hotmail.com <djunior_s@hotmail.com>
2014-08-14 16:41:40 -0300

    Revert "Classe BasicDaoImpl, classe que gerencia todas as entidades junto ao hibernate, foi testando todas as funções com Junit."
    This reverts commit 42a612e218c6b1f78a5c7b2fd8e8f5f55b8a73d8.

--- src/main/java/br/com/projeto/dao/PerfilDao.java
+++ src/main/java/br/com/projeto/dao/PerfilDao.java
@@ -1,20 +1,7 @@
 package br.com.projeto.dao;
 
-import java.util.List;
-
 import br.com.projeto.model.Perfil;
 
 public interface PerfilDao {
-	
 	public void save(Perfil perfil);
-
-	public List<Perfil> getPerfils();
-	
-	public Perfil salvar(Perfil perfil);
-	
-	public void remover(Perfil perfil);
-
-	public Perfil mudarNome(Perfil perfil);
-
-	public Perfil retornaPorId(Long idOfPerfil);
 }
--- src/main/java/br/com/projeto/dao/impl/PerfilDaoImpl.java
+++ src/main/java/br/com/projeto/dao/impl/PerfilDaoImpl.java
@@ -1,42 +1,20 @@
 package br.com.projeto.dao.impl;
 
-import java.util.List;
+import org.hibernate.Session;
 
 import br.com.projeto.dao.PerfilDao;
 import br.com.projeto.model.Perfil;
 
-public class PerfilDaoImpl extends BasicDaoImpl implements PerfilDao{
+public class PerfilDaoImpl implements PerfilDao {
+	
+	private Session session;
+	 
+    public void setSession(Session session) {
+        this.session = session;
+    }
      
     @Override
     public void save(Perfil perfil) {
-        getSession().saveOrUpdate(perfil);
+        this.session.saveOrUpdate(perfil);
     }
-    
-    public List<Perfil> getPerfils(){
-    	return getPureList(Perfil.class, "select perfil from Perfil perfil");
-    }
-    
-    public Perfil salvar(Perfil perfil){
-    	addEntity(Perfil.class, perfil);
-    	return perfil;
-    }
-
-	@Override
-	public void remover(Perfil perfil) {
-		removeEntity(perfil);
-		
-	}
-
-	@Override
-	public Perfil mudarNome(Perfil perfil) {
-		return setEntity(Perfil.class, perfil);
-	}
-
-	@Override
-	public Perfil retornaPorId(Long idOfPerfil) {
-		
-		return getEntity(Perfil.class, idOfPerfil);
-	}
-	
-	
 }
